bea1a2dde8ae83f377be037cb707d460aec8a6ef
hangqianjun <hangqianjun007@gmail.com>
2023-07-11 08:53:08 -0700

    Add unit test coverage for input parameters

--- tests/estimation/test_tomographer.py
+++ tests/estimation/test_tomographer.py
@@ -13,14 +13,21 @@ DS.__class__.allow_overwrite = True
 
 inputdata = os.path.join(RAILDIR, 'rail/examples_data/testdata/output_BPZ_lite.fits')
 
+@pytest.mark.parametrize(
+    "input_param", 
+    [{"zbin_edges": [0.0, 0.3]},
+     {"zmin": 0.0, "zmax": 0.3, "nbins": 1},
+     {"zmin": 0.0, "zmax": 0.3, "nbins": 1, "equal_ngal": True},
+    ]
+)
 
-def test_tomographer():
+def test_tomographer(input_param):
     DS.clear()    
     input_data = DS.read_file('input_data', QPHandle, inputdata)
 
     tomo = naiveClassifierSRD.make_stage(
         point_estimate='zmode',
-        zbin_edges=[0.0,0.3],
+        **input_param,
     )
     
     out_data = tomo.tomography(input_data)
