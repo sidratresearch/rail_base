3683a949ffad51774157b177e24283d4c7c75a03
drewoldag <47493171+drewoldag@users.noreply.github.com>
2023-08-14 15:38:29 -0700

    Adding logic to `CatEstimator` to obey `calculated_point_estimates`.

--- src/rail/estimation/estimator.py
+++ src/rail/estimation/estimator.py
@@ -1,6 +1,10 @@
 """
 Abstract base classes defining Estimators of individual galaxy redshift uncertainties
 """
+from typing import List
+
+import numpy as np
+from numpy.typing import NDArray
 
 from rail.core.common_params import SHARED_PARAMS
 from rail.core.data import TableHandle, QPHandle, ModelHandle
@@ -129,5 +133,107 @@ class CatEstimator(RailStage):
         self._output_handle.set_data(qp_dstn, partial=True)
         self._output_handle.write_chunk(start, end)
 
+    def _calculate_point_estimates(self, qp_dist, grid:NDArray=None) -> dict:
+        """This function drives the calculation of point estimates for qp.Ensembles.
+        It is defined here, and called from the `_process_chunk` method in the
+        CatEstimator child classes.
 
+        Parameters
+        ----------
+        qp_dist : qp.Ensemble
+            The qp Ensemble instance that contains posterior estimates.
+        grid : NDArray, optional
+            The grid on which to evaluate the point estimate. Note that not all
+            point estimates require a grid to be provided, by default None.
 
+        Returns
+        -------
+        dict
+            The ancillary dictionary that contains the point estimates. The possible
+            keys are ['mean', 'mode', 'median'].
+
+        Notes
+        -----
+        If there are particularly efficient ways to calculate point estimates for
+        a given `CatEstimator` subclass, the subclass can implement any of the
+        `_calculate_<foo>_point_estimate` for any of the point estimate calculator
+        methods:
+
+        - `_calculate_mode_point_estimate`
+        - `_calculate_mean_point_estimate`
+        - `_calculate_median_point_estimate`
+        """
+
+        ancil_dict = dict()
+        calculated_point_estimates = []
+        if 'calculated_point_estimates' in self.config:
+            calculated_point_estimates = self.config.calculated_point_estimates
+
+        if 'mode' in calculated_point_estimates:
+            mode_value = self._calculate_mode_point_estimate(qp_dist, grid)
+            ancil_dict.update(mode = mode_value)
+
+        if 'mean' in calculated_point_estimates:
+            mean_value = self._calculate_mean_point_estimate(qp_dist)
+            ancil_dict.update(mean = mean_value)
+
+        if 'median' in calculated_point_estimates:
+            median_value = self._calculate_median_point_estimate(qp_dist)
+            ancil_dict.update(median = median_value)
+
+        return ancil_dict
+
+    def _calculate_mode_point_estimate(self, qp_dist, grid:NDArray=None) -> NDArray:
+        """Calculates and returns the mode values for a set of posterior estimates
+        in a qp.Ensemble instance.
+
+        Parameters
+        ----------
+        qp_dist : qp.Ensemble
+            The qp Ensemble instance that contains posterior estimates.
+        grid : NDArray, optional
+            The grid on which to evaluate the `mode` point estimate, if a grid is
+            not provided, a default will be created at run time using `zmin`, `zmax`,
+            and `nzbins`, by default None
+
+        Returns
+        -------
+        NDArray
+            The mode value for each posterior in the qp.Ensemble
+        """
+        if grid is None:
+            grid = np.linspace(self.config.zmin, self.config.zmax, self.config.nzbins)
+
+        return qp_dist.mode(grid=self.zgrid)
+
+    def _calculate_mean_point_estimate(self, qp_dist) -> NDArray:
+        """Calculates and returns the mean values for a set of posterior estimates
+        in a qp.Ensemble instance.
+
+        Parameters
+        ----------
+        qp_dist : qp.Ensemble
+            The qp Ensemble instance that contains posterior estimates.
+
+        Returns
+        -------
+        NDArray
+            The mean value for each posterior in the qp.Ensemble
+        """
+        return qp_dist.mean()
+
+    def _calculate_median_point_estimate(self, qp_dist) -> NDArray:
+        """Calculates and returns the median values for a set of posterior estimates
+        in a qp.Ensemble instance.
+
+        Parameters
+        ----------
+        qp_dist : qp.Ensemble
+            The qp Ensemble instance that contains posterior estimates.
+
+        Returns
+        -------
+        NDArray
+            The median value for each posterior in the qp.Ensemble
+        """
+        return qp_dist.median()
