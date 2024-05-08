99616e329b036f589d15a3e59db69d5cebcb9e4c
Tianqing Zhang <ztq1996@live.com>
2024-05-08 10:49:52 -0400

    change utils comment

--- src/rail/core/utils.py
+++ src/rail/core/utils.py
@@ -1,6 +1,6 @@
 
 # This is a deprecative path for importing RAILDIR, it is only for the transition period
-# to move RAILDIR to common_params
+# to move RAILDIR and find_rail_file to utils.path_utils
 
 from rail.utils.path_utils import find_rail_file, RAILDIR
 
