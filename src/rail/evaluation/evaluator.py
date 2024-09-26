5b71aade46137a33aa13c6a892ffc74fc3a7e397
Tianqing Zhang <ztq1996@live.com>
2024-09-26 16:59:28 -0400

    remove column that are not computed by the scipy functions

--- src/rail/evaluation/evaluator.py
+++ src/rail/evaluation/evaluator.py
@@ -486,14 +486,19 @@ class OldEvaluator(RailStage):
             # Here we do our best to store the relevant fields in `out_table`.
             if isinstance(value, list):  # pragma: no cover
                 out_table[f"PIT_{pit_metric}"] = value
+            elif pit_metric== 'OutRate':
+                out_table[f"PIT_{pit_metric}_stat"] = [value]
             else:
+
                 out_table[f"PIT_{pit_metric}_stat"] = [
                     getattr(value, "statistic", None)
                 ]
-                out_table[f"PIT_{pit_metric}_pval"] = [getattr(value, "p_value", None)]
-                out_table[f"PIT_{pit_metric}_significance_level"] = [
-                    getattr(value, "significance_level", None)
-                ]
+                if getattr(value, "p_value", None) is not None:
+                    out_table[f"PIT_{pit_metric}_pval"] = [getattr(value, "p_value", None)]
+                if getattr(value, "significance_level", None) is not None:
+                    out_table[f"PIT_{pit_metric}_significance_level"] = [
+                        getattr(value, "significance_level", None)
+                    ]
 
         POINT_METRICS = dict(
             SimgaIQR=PointSigmaIQR,
@@ -516,7 +521,7 @@ class OldEvaluator(RailStage):
         if self.config.do_cde:
             value = CDELoss(pz_data, zgrid, z_true).evaluate()
             out_table["CDE_stat"] = [value.statistic]
-            out_table["CDE_pval"] = [value.p_value]
+            # out_table["CDE_pval"] = [value.p_value]
 
         # Converting any possible None to NaN to write it
         out_table_to_write = {
