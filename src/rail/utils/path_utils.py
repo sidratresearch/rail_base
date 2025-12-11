64883571a27bc80b229f5203179e3eb144432e65
taiwithers <59430904+taiwithers@users.noreply.github.com>
2025-12-11 13:06:41 -0500

    Handle more temporary directory things

--- src/rail/utils/path_utils.py
+++ src/rail/utils/path_utils.py
@@ -24,10 +24,15 @@ def find_rail_file(relpath: str) -> str:
 def unfind_rail_file(abspath: str) -> str:
     """Go backwards from an absolute path to a rail file to the value that was passed to
     `find_rail_file`"""
+    # paths created with find_rail_file
     for path_ in rail.__path__:
         if abspath.startswith(path_):
             rail_package_directory = os.path.abspath(
                 os.path.join(path_, "..", "..", "..")
             )
             return os.path.normpath(abspath.removeprefix(rail_package_directory + "/"))
+
+    # cache locations
+    if abspath.startswith("/home/"):
+        return abspath.replace(os.path.expanduser("~"), "~")
     raise ValueError(f"Could not find the originating source of {abspath}")
